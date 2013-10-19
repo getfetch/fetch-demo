588d1c171bb6e888560ee8ab323db4de84b5a8c0
Joe Esposito <joe@joeyespo.com>
2013-10-19 12:07:28 -0400

    Cleanup app.js.

--- app.js
+++ app.js
@@ -2,29 +2,22 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var path = require('path');
-
 var swig = require('swig');
 
-// This is where all the magic happens!
+// App config
 app.engine('html', swig.renderFile);
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-
-// Swig will cache templates for you, but you can disable
-// that and use Express's caching instead, if you like:
 app.set('view cache', false);
-// To disable Swig's cache, do the following:
 swig.setDefaults({ cache: false });
-// NOTE: You should always cache templates in a production environment.
-// Don't leave both of these to `false` in production!
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Routes
 app.get('/', function (request, response) {
   response.render('index', { /* template locals context */ });
 });
 
+// Run server
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Listening on ' + port);
