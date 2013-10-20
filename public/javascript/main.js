1853be382d3d770a14b736f274db9eb9a03533d5
Andrew Miller <amcoder@gmail.com>
2013-10-19 22:17:44 -0400

    Finish filter form layout

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,6 +3,7 @@ $(function() {
   $('#main-menu').mmenu();
   $('#main-menu-link').click(function() {
     $('#main-menu').trigger('open');
+    return false;
   });
 
   $('#filter-settings').mmenu({
@@ -10,7 +11,11 @@ $(function() {
   });
   $('#filter-link').click(function() {
     $('#filter-settings').trigger('open');
+    return false;
   });
 
   $('#size').buttonset();
+  $('#age').buttonset();
+  $('#distance').buttonset();
+  $('#sex').buttonset();
 });
