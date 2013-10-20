24e87b32e765ee12e408fd327b30b746a0c99b48
Chris <chris.wargula@gmail.com>
2013-10-20 09:32:09 -0400

    Added more org data to datasource

--- organization.js
+++ organization.js
@@ -4,17 +4,17 @@ function load(id, callback){
     var model;
     
     for(var key in dogs){
-        if(dogs[key].shelter.id == id){
-            var shelter = dogs[key].shelter;
+        if(dogs[key].organization.id == id){
+            var organization = dogs[key].organization;
             model = {
-                id: shelter.id,
-                name: shelter.name,
-                type: 'Breeder',
-                years_in_biz: '10 yrs',
-                location: '123 Main St Pittsburgh, PA 15220',
-                email: 'adoption@beaglerescue.com',
-                phone: '321.321.3211',
-                frequency: 'frequency',
+                id: organization.id,
+                name: organization.name,
+                type: capitaliseFirstLetter(organization.type),
+                years_in_biz: organization.yrs_in_biz,
+                location: organization.address,
+                email: organization.email,
+                phone: organization.phone,
+                frequency: organization.frequency,
                 photos: [
                     { link: '1.jpg' },
                     { link: '2.jpg' },
@@ -28,6 +28,12 @@ function load(id, callback){
     }
 
     callback(model);
+}
+
+function capitaliseFirstLetter(string)
+{
+    string = string.toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 module.exports.load = load;
