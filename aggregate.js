40a655cb1520d547da8716bb4390efc752bacb19
Joe Esposito <joe@joeyespo.com>
2013-10-19 22:28:44 -0400

    Add TODOs to query.

--- aggregate.js
+++ aggregate.js
@@ -7,6 +7,8 @@ var API_KEY = 'c05eebea71cf26cfa156b08689269176';
 var API_SECRET = 'b0f53f55688e39e093bdd43fab8f61c5';
 
 function request(zipcode, callback) {
+  // TODO: Distance option
+  // TODO: Filters
   var query = querystring.stringify({key: API_KEY, format: 'json', animal: 'dog', location: zipcode});
   var signature = crypto.createHash('md5').update(API_SECRET + query).digest('hex');
   
