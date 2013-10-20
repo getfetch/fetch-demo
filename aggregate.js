9660bffdd70f0b7d76a8db226ead0bd0eca8c135
Joe Esposito <joe@joeyespo.com>
2013-10-19 22:28:00 -0400

    Query for dogs.

--- aggregate.js
+++ aggregate.js
@@ -7,7 +7,7 @@ var API_KEY = 'c05eebea71cf26cfa156b08689269176';
 var API_SECRET = 'b0f53f55688e39e093bdd43fab8f61c5';
 
 function request(zipcode, callback) {
-  var query = querystring.stringify({key: API_KEY, format: 'json', location: zipcode});
+  var query = querystring.stringify({key: API_KEY, format: 'json', animal: 'dog', location: zipcode});
   var signature = crypto.createHash('md5').update(API_SECRET + query).digest('hex');
   
   var content = '';
